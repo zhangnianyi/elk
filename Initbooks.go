1a9f73e8ae2807786e9090b45873c9dc7e5bf50d
zhangnianyi <4721156@qq.com>
2021-01-04 17:00:24 +0800

    add go func() insert to es for 20000

--- Initbooks.go
+++ Initbooks.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"elk/AppInit"
+	"github.com/olivere/elastic/v7"
 	"elk/Models"
 	"fmt"
 	"log"
 	"strconv"
+	"sync"
 )
 
 func main() {
 	pasge :=1
-	pagezise :=50
+	pagezise :=500
+	wg :=sync.WaitGroup{}
 	for{
 		//读取50条到booklist里面
 		book_list :=Models.BookList{}
@@ -19,18 +22,43 @@ func main() {
 		if db.Error !=nil  ||len(book_list) ==0{
 			break
 		}
-		//fmt.Println(book_list[2])
-		//遍历book list插入到es
-		for _,book :=range book_list{
-			ctx :=context.Background()
-			rsp,err :=AppInit.Esclient().Index().Index("books").
-				Id(strconv.Itoa(book.BookID)).BodyJson(book).Do(ctx)
+		wg.Add(1)   //开启携程
+		//client :=AppInit.Esclient()
+		//bulk :=client.Bulk()
+		////fmt.Println(book_list[2])
+		////遍历book list插入到es
+		//for _,books :=range book_list{
+		//	rqe :=elastic.NewBulkIndexRequest()
+		//	rqe.Index("books").Id(strconv.Itoa(books.BookID).Doc(books))
+		//	bulk.Add(rqe)
+		//	//ctx :=context.Background()
+		//	//rsp,err :=AppInit.Esclient().Index().Index("books").
+		//	//	Id(strconv.Itoa(book.BookID)).BodyJson(book).Do(ctx)
+		//	//if err !=nil{
+		//	//	log.Fatal(err)
+		//	//}
+		//	//fmt.Println(rsp)
+		//}
+		//rsp,err :=bulk.Do(context.Background())
+		go func() {
+			defer  wg.Done()  //很重要
+			client:=AppInit.Esclient()   //打开一个es的链接
+			bulk:=client.Bulk()  //采用bulk的方式插入
+			for _,book:=range book_list {
+
+				req:=elastic.NewBulkIndexRequest().Index("books").
+					Id(strconv.Itoa(book.BookID)).Doc(book)
+				bulk.Add(req)
+
+			}
+			rsp,err:=bulk.Do(context.Background())  //执行这个bulk
 			if err !=nil{
 				log.Fatal(err)
+			}else {
+				fmt.Println(rsp)
 			}
-			fmt.Println(rsp)
-		}
-
-		break
+		}()
+		pasge=pasge+1   //这个必须加 不要跳循环了跳转不到第二页
 	}
+		wg.Wait()
 }
